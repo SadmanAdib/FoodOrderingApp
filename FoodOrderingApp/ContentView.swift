9561cdac537ad7fecd1da470f96737f8d67a2589
Sadman Adib <sadmanadib67@gmail.com>
2022-06-30 11:12:59 +0600

    Initial Commit

---
+++ FoodOrderingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodOrderingApp
+//
+//  Created by Sadman Adib on 30/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodOrderingApp/FoodOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderingAppApp.swift
+//  FoodOrderingApp
+//
+//  Created by Sadman Adib on 30/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
