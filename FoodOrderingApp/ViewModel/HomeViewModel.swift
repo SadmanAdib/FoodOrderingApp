7b676753a9843e2716964a012fce8c4ee9500e56
Sadman Adib <sadmanadib67@gmail.com>
2022-06-30 11:52:02 +0600

    init

---
+++ FoodOrderingApp/ViewModel/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  FoodOrderingApp
+//
+//  Created by Sadman Adib on 30/6/22.
+//
+
+import SwiftUI
+
+class HomeViewModel: ObservableObject{
+    
+    @Published var search = ""
+    
+}
